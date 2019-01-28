3885407a92df6169f97e7fa910ca488ed797372b
Odell <odell.purifoy@gmail.com>
2019-01-27 20:33:50 -0500

    Start of authentication logic in Application controller and signup controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  # hook into JWTSessions
+  include JWTSessions::RailsAuthorization
+  rescue_from JWTSessions::Errors::Unauthorized, with :not_authorized
+
+  #Private method to rescue Unauthorized errors
+  private
+    #method to access current user
+    def current_user
+      @current_user ||= User.find(payload['user_id'])
+    end
+
+    def not_authorized
+      render json: { error: 'Not Authorized' }, status: :unauthorized
+    end
 end
